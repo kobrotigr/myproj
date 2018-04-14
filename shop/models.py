7d7b68c12efbc136a514224f232aec001441322d
evol <maxmko.vk@gmail.com>
2018-04-14 13:25:58 +0300

    added 1st template view

--- shop/models.py
+++ shop/models.py
@@ -2,25 +2,15 @@ from django.db import models
 from django.utils import timezone
 
 
-class Shop(models.Model):
+class Book(models.Model):
     User = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    book_author = models.CharField(max_length=50)
-    book_title = models.CharField(max_length=200)
-    book_price = models.IntegerField()
-    book_comment = models.TextField()
-    basket = []
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=200)
+    price = models.IntegerField()
+    ISBN = models.CharField(max_length=15)
+    comment = models.TextField()
     created_date = models.DateTimeField(
         default=timezone.now)
     published_date = models.DateTimeField(
         blank=True, null=True)
 
-    def add_basket(self, book_title, basket):
-        basket.append(str(book_title))
-        self.published_date = timezone.now()
-        self.save()
-
-    def calculate_average_price(self):
-        pass
-
-    def __str__(self):
-        return self.book_title
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+from .models import Book
+from django.views.generic import ListView
+
+
+class BookList(ListView):
+    model = Book
+    template_name = 'shop/shop_list.html'
+    queryset = Book.objects.all()
+    context_object_name = 'books'
+
